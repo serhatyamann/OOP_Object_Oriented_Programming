08240154f0d58697e7e992e5994cffaee8ebf7e7
serha <serha@VERITAS-PC>
2021-09-27 18:51:03 +0300

    Add project files.

---
+++ Encapsulation/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encapsulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Product prod = new Product();
+            prod.ProdName = "Keyboard";
+            prod.ProdDescription = "Wireless Keyboard";
+            prod.ProdPrice = 50;
+            prod.ProdStock = 0;
+        }
+    }
+}
---
+++ Encapsulation/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Product
+    {
+        public string ProdName { get; set; }
+        public string ProdDescription { get; set; }
+        public int ProdPrice { get; set; }
+        private int _prodStock;
+        public int ProdStock
+        {
+            get
+            {
+                return _prodStock;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _prodStock = value;
+                    System.Windows.Forms.MessageBox.Show("Product successfully added..!");
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("You must enter a bigger number than '0' for stock..!");
+                }
+            }
+        }
+        public Product() { }
+    }
+}
