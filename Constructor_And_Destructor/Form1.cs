d8a0ffe68bb7f2334f2de5962357f20ee95f3494
Serhat YAMAN <serhatyaman@hotmail.com.tr>
2021-09-29 02:12:50 +0300

    Updated Solution

---
+++ Constructor_And_Destructor/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Constructor_And_Destructor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            User user1 = new User();
+            User user2 = new User(3);
+            User user3 = new User(2, "Serhat Yaman", 27);
+
+        }
+    }
+}
---
+++ Constructor_And_Destructor/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Constructor_And_Destructor
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        
+        public User()
+        {
+            MessageBox.Show("An Instance of class created.");
+        }
+        //Overloaded one.
+        public User(int _id)
+        {
+            this.ID = _id;
+            MessageBox.Show($"ID: {this.ID}");
+        }
+        //Overloaded another one.
+        public User(int _id, string _fullName, int _age)
+        {
+            this.ID = _id;
+            this.FullName = _fullName;
+            this.Age = _age;
+            MessageBox.Show($"ID: {this.ID}\nFull Name: {this.FullName}\nAge: {this.Age}");
+        }
+        //Destructor
+        ~User()
+        {
+            MessageBox.Show("An Instance of class destroyed");
+        }
+    }
+}
